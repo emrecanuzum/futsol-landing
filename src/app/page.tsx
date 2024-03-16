62a49044bccd11f21da2b1a723fb5bbc6e7a3298
emrecanuzum <uzumemrecan@gmail.com>
2024-03-16 13:41:02 +0300

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,17 @@ import Link from "next/link";
 
 const LandingPage = () => {
   return (
-    <div className="bg-[#160023] h-screen">
+    <div className="bg-[#160023] overflow-hidden h-screen">
       <div className=" justify-center  flex max-w-screen">
-        <div className="md:w-full bg-[#160023] w-[150vw]  md:min-h-screen  text-auto justify-center md:flex">
+        <div className="md:w-full bg-[#160023] w-screen  md:min-h-screen  text-auto justify-center md:flex">
           <Image
-            className=" scale-[3] overflow-auto block top-1/3 md:top-0  aspect-video  md:scale-100 md:relative w-screen md:w-[96vw] mt-10 "
+            className=" scale-[3] mt-[75%] top-1/2 md:top-0  aspect-video  md:scale-100 md:relative w-screen md:w-[96vw] md:mt-10 "
             src={FootballField}
             alt="football-field"
           />
-          <div className="fixed gap-3 flex top-5">
+          <div className="w-full fixed gap-3 md:left-auto mx-auto justify-center flex top-5">
             <Link
-              className=" shadow-glow md:bg-opacity-35 bg-black hover:bg-white hover:text-black text-white shadow-white rounded-lg flex border p-2 top-10 items-center"
+              className=" shadow-glow hover:animate-spin md:bg-opacity-35 bg-black hover:bg-white hover:text-black text-white shadow-white rounded-lg flex border p-2 top-10 items-center"
               href={"https://twitter.com/playfutsol"}
               target="blank"
             >
@@ -32,7 +32,7 @@ const LandingPage = () => {
               target="blank"
             >
               <FaMedium className="" size={20} />
-              <p className=" font-thin pl-1">futsolgame</p>
+              <p className=" font-thin pl-1">playfutsol</p>
             </Link>
           </div>
 
