2eb8417e42e93af88e12ac2bf4dea65dcb4306f6
emrecanuzum <uzumemrecan@gmail.com>
2024-03-16 05:45:30 +0300

    ssr update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import FootballField from "../../public/field.png";
+import MiddleText from "./components/middle-text";
 
 const LandingPage = () => {
   return (
@@ -12,33 +13,7 @@ const LandingPage = () => {
             src={FootballField}
             alt="football-field"
           />
-          <h1
-            className="text-[#160023]   cursor-default flex flex-col text-center pt-16 text-4xl md:text-6xl z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-            style={{
-              transition: "0.3s",
-            }}
-            onMouseOver={(e) => {
-              e.currentTarget.style.textShadow =
-                "0 0 8px rgba(255,255,255,0.8), 0 0 10px rgba(255,255,255,0.8), 0 0 12px rgba(255,255,255,0.8), 0 0 14px rgba(255,255,255,0.8)";
-            }}
-            onMouseOut={(e) => {
-              e.currentTarget.style.textShadow = "";
-            }}
-          >
-            We will be here{" "}
-            <span
-              className="uppercase text-5xl md:text-7xl"
-              style={{ transition: "transform 0.3s ease-in-out" }}
-              onMouseOver={(e) => {
-                e.currentTarget.style.transform = "scale(1.5)";
-              }}
-              onMouseOut={(e) => {
-                e.currentTarget.style.transform = "scale(1)";
-              }}
-            >
-              soon
-            </span>
-          </h1>
+          <MiddleText />
         </div>
       </div>
     </div>
