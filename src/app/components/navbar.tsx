6871a595675f789f261f1e91e6c06569ddf772d0
emrecanuzum <uzumemrecan@gmail.com>
2024-03-17 03:50:59 +0300

    layout goes well

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 
 const NavbarComponent = () => {
+  // Double the number of items for a smooth looping effect
+  const items = Array.from({ length: 10 }); // Create an array with 20 items
+
   return (
-    <div className=" md:h-screen md:sticky bg-aquagreen">NavbarComponent</div>
+    <div className="overflow-hidden md:h-screen md:sticky bg-aquagreen">
+      <div
+        className="grid w-full px-4 gap-6 justify-center pt-4"
+        style={{
+          animation: "smoothScroll 10s linear infinite",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {items.map((_, index) => (
+          <div
+            key={index}
+            className="border bg-black bg-opacity-80 text-aquagreen px-12 text-5xl h-32 flex text-center justify-center items-center border-black"
+          >
+            PLAYER NFT
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
